4fd7743854e32074d267bd0bbde01b5f82e90aec
neotan <neotan12@hotmail.com>
2022-05-15 13:47:21 -0400

    fix: use new formatDate util to format datetime

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,16 @@
+import format from 'date-fns/format'
+import toDate from 'date-fns/toDate'
+
 export function noop() {
   console.log('[noop]: empty function execcuted')
 }
 
 export function isBrowser() {
   return typeof window !== 'undefined'
+}
+
+export function formatDate(date, dateFormat = 'yyyy-MM-dd') {
+  return date instanceof Date
+    ? format(toDate(date), dateFormat)
+    : format(new Date(date), dateFormat)
 }
