01f33266d0ad657b9a652bbe0cef82ac94647e61
neotan <neotan12@hotmail.com>
2022-05-16 00:38:37 -0400

    fix: add apps folder and update home page

---
+++ pages/apps/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '~/components/layout'
+
+export default function Apps() {
+  return (
+    <Layout>
+      <h1 className="text-center text-[10rem]">Apps</h1>
+    </Layout>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from 'components/layout'
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1 className=" text-center text-[10rem]">Home</h1>
     </Layout>
   )
 }
