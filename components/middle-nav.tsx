d5afaeb420eec645b59cf97370684e76faa4f8b2
neotan <neotan12@hotmail.com>
2022-05-16 00:39:44 -0400

    fix: add red bottom border for active nav item

--- components/middle-nav.tsx
+++ components/middle-nav.tsx
@@ -1,10 +1,9 @@
 import {ReactNode} from 'react'
 import Link from 'next/link'
-import {
-  BookOpenIcon,
-  LightningBoltIcon,
-  TagIcon,
-} from '@heroicons/react/outline'
+import {useRouter} from 'next/router'
+import {twMerge} from 'tailwind-merge'
+import clsx from 'clsx'
+import {BookOpenIcon, LightningBoltIcon} from '@heroicons/react/outline'
 
 type NavMidItemProps = {
   title?: ReactNode
@@ -13,9 +12,18 @@ type NavMidItemProps = {
 }
 
 function NavItem({title, href = '/', icon: Icon}: NavMidItemProps) {
+  const {pathname} = useRouter()
+  const isActive = href === pathname
   return (
-    <Link href={href} passHref>
-      <a className="sm:w-26 bg-link group flex h-10 cursor-pointer items-center space-x-2 rounded-md p-3 transition  sm:space-x-0 sm:hover:scale-110">
+    <Link href={href}>
+      <a
+        className={twMerge(
+          'sm:w-26 bg-link flex h-10 cursor-pointer items-center space-x-2 rounded-md border-b-8 border-b-transparent p-4 pt-5 transition sm:space-x-0',
+          clsx({
+            'rounded-b-none border-b-red-600': isActive,
+          }),
+        )}
+      >
         <Icon className="mr-1 hidden h-5 sm:inline-block" />
         <h2 className="text-xl font-bold">{title}</h2>
       </a>
@@ -26,9 +34,8 @@ function NavItem({title, href = '/', icon: Icon}: NavMidItemProps) {
 export default function MiddleNav() {
   return (
     <div className="flex max-w-2xl flex-grow justify-evenly">
-      <NavItem title="BLOGS" icon={BookOpenIcon} href={'/blogs'} />
-      <NavItem title="PRODUCTS" icon={LightningBoltIcon} href={'/products'} />
-      <NavItem title="TAGS" icon={TagIcon} href={'/tags'} />
+      <NavItem title="BLOGS" icon={BookOpenIcon} href="/blogs" />
+      <NavItem title="APPS" icon={LightningBoltIcon} href="/apps" />
     </div>
   )
 }
