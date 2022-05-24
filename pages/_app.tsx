5dd3e06c67b818125878f9d5aaa56d583e968c8a
neotan <neotan12@hotmail.com>
2022-05-24 11:09:02 -0400

    feat: add SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import {DefaultSeo} from 'next-seo'
 import {ThemeProvider} from 'next-themes'
 
 function CustomApp({Component, pageProps}) {
@@ -9,6 +10,26 @@ function CustomApp({Component, pageProps}) {
         <title>NEOTAN.ME</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <DefaultSeo
+        titleTemplate="%s - Neo Tan"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'neotan.me',
+          description:
+            'The personal website for Neo Tan, web software engineer.',
+          site_name: 'Neo Tan | neotan.me',
+          images: [
+            {
+              url: 'https://www.neotan.me/images/avatar-icon.svg',
+              width: 600,
+              height: 600,
+              alt: 'Og Image Alt',
+            },
+          ],
+        }}
+        canonical="neotan.me"
+      />
       <ThemeProvider
         attribute="class"
         storageKey="neotan.me-theme"
