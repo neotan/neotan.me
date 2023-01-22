ed7056830be5891a7287c822c568c3adf37c666a
neotan <neotan12@hotmail.com>
2023-01-22 05:56:25 +0000

    feat(apps/web3): add GetStarted section

--- web3/app/page.tsx
+++ web3/app/page.tsx
@@ -226,9 +226,3 @@ export default function Web3Page() {
     </>
   )
 }
-
-export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
-];
